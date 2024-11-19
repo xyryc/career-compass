82be7f766f984b7b55d32a6df0418ea3200cee0d
xyryc <amdanikzz@gmail.com>
2024-11-19 13:28:55 +0600

    json data updated

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,12 +1,24 @@
 /* eslint-disable react/prop-types */
-import { FaUserAlt } from "react-icons/fa";
+import { FaRegStar, FaUserCircle } from "react-icons/fa";
+import { MdLanguage } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
-  const { id, image, service_name, category, price, counselor } = service;
+  const {
+    id,
+    image,
+    service_name,
+    category,
+    description,
+    price,
+    rating,
+    counselor,
+    language,
+
+  } = service;
 
   return (
-    <div className="card bg-base-100 shadow-xl border rounded-lg p-4">
+    <div className="card bg-base-100 border rounded-lg p-4">
       {/* Service Image */}
       <figure>
         <img
@@ -17,19 +29,38 @@ const ServiceCard = ({ service }) => {
       </figure>
       {/* Service Details */}
       <div className="card-body p-4">
-        <div>
-          <h2 className="card-title text-lg font-bold">{service_name}</h2>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+        <div className="flex items-center gap-6">
+          <div className="flex items-center text-gray-500 text-sm">
+            <FaUserCircle className="mr-2" />
+            <p>{counselor || "Mr. Stephen Doe"}</p>
+          </div>
+          <div className="flex items-center text-gray-500 text-sm">
+            <p className="badge badge-info badge-outline">
+              {category || "Online"}
+            </p>
           </div>
         </div>
-        <p className="text-sm text-gray-500">Category: {category}</p>
-        <p className="text-sm text-gray-500">Price: ${price}</p>
-        <div className="flex items-center mt-2">
-          <FaUserAlt className="text-blue-500 mr-2" />
-          <p className="text-sm text-gray-500">{counselor}</p>
+        <h2 className="card-title text-lg font-bold">{service_name}</h2>
+        <p className="text-sm text-gray-600">{description.slice(0, 85)}...</p>
+
+        <div className="border-b-[1px] mt-3"></div>
+
+        <div className="flex items-center justify-between">
+          <p className="text-sm text-gray-600 font-medium flex items-center gap-1">
+            <MdLanguage />
+            {language || "Russian"}
+          </p>
+          <p className="text-sm"></p>
         </div>
+
+        <div className="flex items-center justify-between">
+          <p className="text-sm text-gray-600 font-medium">Cost: ${price}</p>
+          <p className="text-sm text-gray-600 font-medium flex items-center gap-1">
+            <FaRegStar />
+            {rating || "4.6"}
+          </p>
+        </div>
+
         <Link
           to={`/service-details/${id}`}
           className="btn btn-outline mt-4 w-full"
